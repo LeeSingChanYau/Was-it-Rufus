80ed76e5fe81639747b016d89ccaba8bf13d3299
lee sing chan <leesingchan@Lees-MacBookPro14.local>
2023-01-13 19:20:48 -0800

    Finished implementing the four main functionalities.

--- index.js
+++ index.js
@@ -17,10 +17,37 @@ exec('git diff HEAD', { cwd: git_dir}, (err, stdout, stderr) => {
     if (err) {
         console.log(err);
     }
-    console.log(stdout);
     if (stdout === '') {
         console.log("local changes: False");
     } else {
         console.log("local changes: True");
     }
-})
+});
+
+exec('git log -1 --format=%ct', { cwd: git_dir}, (err, stdout, stderr) => {
+    if (err) {
+        console.log(err);
+    }
+    
+    const date = new Date(stdout * 1000);
+    const oneWeekAgo = new Date(date.setDate(date.getDate() - 7));
+    const lastCommitDate = new Date(stdout * 1000);
+    
+    if (lastCommitDate > oneWeekAgo) {
+        console.log("recent commit: True");
+    } else {
+        console.log("recent commit: False");
+    }
+});
+
+exec(`git log -1 --pretty=format:'%an'`, { cwd: git_dir}, (err, stdout, stderr) => {
+    if (err) {
+        console.log(err);
+    }
+    
+    if(stdout==="Rufus") {
+        console.log("blame Rufus: True");
+    } else {
+        console.log("blame Rufus: False");
+    }
+});
