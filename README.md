d37e8ac9cfb7987f82775cb7752a271234a17746
lee sing chan <leesingchan@Lees-MacBookPro14.local>
2023-01-13 19:39:05 -0800

    Wrote the instructions in the README.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Was-it-Rufus
 A program that prints specific facts about a local git repository. 
+
+# How to run
+1. Have npm installed
+2. git clone this project
+3. go to the local directory of the project, and the call this command in the terminal:
+    node index.js your/git/directory
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { exec } = require('child_process');
-const git_dir = '/Users/leesingchan/Documents/web/Was-it-Rufus';
+const args = process.argv;
+const git_dir = args[2]; 
 
 exec('git rev-parse --abbrev-ref HEAD', { cwd:  git_dir}, (err, stdout, stderr) => {
     if (err) {
-        // handle your error
         console.log(err);
     }
-    
     if (typeof stdout === 'string') {
       console.log("active branch:", stdout);
-      // Call your function here conditionally as per branch
     }
 });
 
